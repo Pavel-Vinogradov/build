89cbf0e2895e225f5db88ac44af7f90a108b1359
Pavel <65782910+Pavel-Vinogradov@users.noreply.github.com>
2020-11-08 00:23:45 +0300

    add block  card feedback

--- app/js/main.js
+++ app/js/main.js
@@ -41,3 +41,35 @@ const mySwiper = new Swiper('.swiper-container', {
   // And if we need scrollbar
 
 });
+
+
+
+
+const priceItem = document.querySelectorAll('.price__item');
+const mainTitle = document.querySelector('.main-text__title');
+const mainItem = document.querySelectorAll('.main-text__item');
+const sectionTitle = document.querySelectorAll('.section__title');
+
+sectionTitle.forEach(item => {
+  item.classList.add('wow', 'fadeInUp')
+});
+
+mainTitle.classList.add('wow', 'fadeInUp');
+
+for (let i = 0, j = 0; i < mainItem.length, j < 0.6; i++, j = j + 0.2) {
+  const element = mainItem[i];
+  element.classList.add('wow', 'fadeInLeft');
+  element.setAttribute('data-wow-delay', `${j}s`);
+
+}
+
+
+
+
+
+for (let i = 0, j = 0; i < priceItem.length, j < 1.2; i++, j = j + 0.2) {
+  const element = priceItem[i];
+  element.classList.add('wow', 'fadeInUp');
+  element.setAttribute('data-wow-delay', `${j}s`);
+
+}
